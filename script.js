4fc9bcfe06363451b010475ff296a4fab85fdcce
Ben Ryan <50218096+benryan03@users.noreply.github.com>
2020-10-30 21:07:01 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+//var horizontalDistance = 740;
+
+const image = document.getElementById('player');
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+
+const player = {
+	w: 40,
+	h: 40,
+	x: 20,
+	y: 20,
+	speed: 10,
+	dx: 0,
+	dy: 0
+};
+
+function drawPlayer(){
+	ctx.drawImage(image, player.x, player.y, player.w, player.h);
+}
+
+function clear(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function newPos(){
+	player.x += player.dx;
+	player.y += player.dy;
+
+	detectWalls();
+}
+
+function detectWalls(){
+	// Left
+	if (player.x < 0){
+		player.x = 0;
+	}
+	// Right
+	if (player.x + player.w > canvas.width){
+		player.x = canvas.width - player.w;
+	}
+	// Top
+	if (player.y < 0){
+		player.y = 0;
+	}
+	// Bottom
+	if (player.y + player.h > canvas.height){
+		player.y = canvas.height - player.w;
+	}
+}
+
+function update(){
+
+	clear();
+
+	drawPlayer();
+
+	newPos();
+
+	requestAnimationFrame(update);
+}
+
+function moveUp(){
+	player.dy = -player.speed;
+}
+
+function moveDown(){
+	player.dy = player.speed;
+}
+
+function moveRight(){
+	player.dx = player.speed;
+}
+
+function moveLeft(){
+	player.dx = -player.speed;
+}
+
+function keyDown(e){
+	console.log(e.key);
+	if (e.key == 'ArrowRight' || e.key == 'Right'){
+		moveRight();
+	}
+	else if (e.key == 'ArrowLeft' || e.key == 'Left'){
+		moveLeft();
+	}
+	else if (e.key == 'ArrowUp' || e.key == 'Up'){
+		moveUp();
+	}
+	else if (e.key == 'ArrowDown' || e.key == 'Down'){
+		moveDown();
+	}
+}
+
+function keyUp(e){
+	if (
+		e.key == 'Right' ||
+		e.key == 'ArrowRight' ||
+		e.key == 'Left' ||
+		e.key == 'ArrowLeft' ||
+		e.key == 'Up' ||
+		e.key == 'ArrowUp' ||
+		e.key == 'Down' ||
+		e.key == 'ArrowDown'
+	){
+		player.dx = 0;
+		player.dy = 0;
+	}
+}
+
+update();
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+
+
+
+
+
